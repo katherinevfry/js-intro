bab569c10e5074577f3a575ea3da8261bb25beb2
Katy-fry <katyvfry@gmail.com>
2021-01-16 13:09:18 -0600

    worked through conditional logic

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log("hello worlddd")
+// console.log("hello worlddd")
+
+// function thisIsTrue(arg1, arg2) {
+// if (arg1 === 'nice' && arg2 === 'Nice') {
+//   console.log(`you are ${arg1}`);
+//   } else {
+//     console.log('false');
+//   }
+// }
+
+// thisIsTrue('nice', 'Nice', 'nice nice'); //JS will ignore the third argument bc it is only looking for two as defined in the function
+
+// function thisIsTrue(arg1, arg2) {
+//   if (arg1.length < 3) {
+//     console.log(`you are ${arg1}`);
+//     } else if (arg2.length > 8) {
+//       console.log(`you are ${arg2}`); //both if and else if are true but it exists the code block at the first true statement it finds.
+  //   } else {
+  //     console.log('you are nothing!');
+  //   }
+  // }
+
+  // thisIsTrue('ni', 'mickeymouse')
+
+
+  // const canDrive = (age) => {
+  //   if (age >= 15) {
+  //     return true;
+  //   } 
+
+  //   return false; //JS reads top to bottom. if the if statement is true, it exits the block. the default return is false unless the if statement is true.
+  // }
+
+  // console.log(canDrive(15));
