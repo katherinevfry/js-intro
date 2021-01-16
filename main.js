4f3edb8bda93fc182dbea131941a80a85dc43261
Katy-fry <katyvfry@gmail.com>
2021-01-16 11:08:01 -0600

    added variables, data types and math to js file

--- main.js
+++ main.js
@@ -1,1 +1,59 @@
-console.log("hello worlddd")
+// console.log("hello worlddd")
+
+
+//************* START VARIABLES *************
+// let firstName = 'Beyonce';
+// let lastName = 'Knowles';
+
+// console.log(`${firstName} ${lastName}`);
+
+// firstName = 'Katy';
+// lastName = 'Fry'
+
+// console.log(`${firstName} ${lastName}`);
+
+// const favoriteFood = 'Tacos';
+
+// console.log(favoriteFood);
+
+// favoriteFood = 'Hamburger';
+
+// console.log(favoriteFood);
+
+// *********END VARIABLES*********
+
+
+// ********* START DATA TYPES**********
+
+// const dog = 'bark';
+// console.log(typeof dog);
+
+// const answer= 33;
+// const negative = -14;
+
+// console.log(typeof answer, typeof negative);
+
+// const iLoveJS = true;
+
+// console.log(typeof iLoveJS);
+
+// ********* END DATA TYPES ************
+
+// ********* START MATH ************
+
+// console.log(Math.pow(2,2)) //4
+// console.log(Math.pow(3,2)) //9
+// console.log(Math.pow(3,3)) //27
+
+// console.log(Math.round(6.5)) //7
+// console.log(Math.round(6.45)) //6
+
+// console.log(Math.floor(6.9999)) //6
+// console.log(Math.ceil(6.000001)) //7
+
+const cat = 'kitty';
+
+console.log(cat.length); //5
+
+
+// ********* END MATH ***********
